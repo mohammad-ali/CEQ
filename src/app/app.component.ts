fc594de511de0d53abd3206800743e69b96f11ea
mohammadali <mohammadalikazeminejad@gmail.com>
2021-10-26 22:10:59 +0330

    header semi final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as jquery from 'jquery';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'CEQ';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    $(document).ready(function () {
+      alert('sss');
+    });
+  }
 }
