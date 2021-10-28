d659b51ce05d528d67d60ca5d2f02a3459584ef2
amir <amirhoseinif@gmail.com>
2021-10-28 12:43:07 +0200

    second update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
     $(document).ready(function () {
       $('.carousel').carousel();
       $('.tabs').tabs();
+      $('.collapsible').collapsible();
     });
   }
 }
